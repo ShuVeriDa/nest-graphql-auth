c7c37f0ee976166a1dd21089399f7f5ffae1d7fd
ShuVeriDa <bashtarov@outlook.com>
2023-08-05 22:58:56 +0300

    second commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [PassportModule, UsersModule],
+  providers: [LocalStrategy, AuthService, AuthResolver],
+})
+export class AuthModule {}
